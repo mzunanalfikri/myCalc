c423caec8c18d32d31630153c8a2f045842621da
Anindya <pamsrewari@gmail.com>
2020-03-21 18:52:28 +0700

    Ngebenerin format penulisan

---
+++ Calculator_Scientific/Calculator_Scientific/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculator;
+
+namespace Calculator_Scientific
+{
+    public partial class Form1 : Form
+    {
+
+        Calc calc = new Calc();
+        string current_number; // menyimpan angka dari input pengguna
+        bool AssignClicked = false; // menandai apakah tombol = sudah ditekan atau belum
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Jika tombol angka diklik
+        private void Number_Clicked(object sender, EventArgs e)
+        {
+            Button clicked = (Button)sender;
+
+            // Menghapus angka di layar jika di layar 0 atau setelah tanda = ditekan
+            if (textBox_Result.Text == "0" || AssignClicked)
+            {
+                if (AssignClicked)
+                {
+                    textBox_Result.Clear();
+                    AssignClicked = false;
+                }
+                else if (clicked.Text != ".")
+                {
+                    textBox_Result.Clear();
+                }
+            }
+
+            // Mengubah tulisan di layar dan menyimpan angka ke current_number
+            textBox_Result.Text = textBox_Result.Text + clicked.Text;
+            current_number = current_number + clicked.Text;
+
+            // Tombol yang terakhir di tekan bukan tombol operator, 
+            // maka isOperation = false
+            calc.SetStateOperation(false);
+        }
+
+        // Jika tombol operator diklik
+        // TODO: Yang paham materi Exception, bisa tambahin exc kalo 
+        // pengguna menginput button_operator secara tidak wajar :v
+        private void Operator_Clicked(object sender, EventArgs e)
+        {
+            Button clicked = (Button)sender;
+            // mengubah isOperation menjadi true karena tombol operator diklik
+            calc.SetStateOperation(true);
+            
+            // Menyimpan operator yang diklik ke operatorSign
+            calc.SignOperator(clicked.Text);
+
+            // Menyimpan angka ke operand1
+            calc.SetOperand1(current_number);
+            current_number = "";
+
+            // Menampilkan operator ke layar
+            textBox_Result.Text = textBox_Result.Text + clicked.Text;
+        }
+
+        private void asignButton(object sender, EventArgs e)
+        {
+            AssignClicked = true;
+
+            // Menyimpan angka ke operand 2
+            calc.SetOperand2(current_number);
+            current_number = "";
+
+            // Menghitung operasi
+            double temp = calc.calculate();
+
+            // Menampilkan hasil operasi
+            textBox_Result.Clear();
+            textBox_Result.Text = temp.ToString();
+        }
+
+        private void AC_button(object sender, EventArgs e)
+        {
+            calc.Reset();
+            textBox_Result.Text = "0";
+        }
+
+        private void MC_Button(object sender, EventArgs e)
+        {
+            calc.SetMemory(textBox_Result.Text);
+            textBox_Result.Text = "0";
+        }
+
+        private void MR_Button(object sender, EventArgs e)
+        {
+            calc.GetMemory();
+        }
+    }
+}
---
+++ Calculator_Scientific/Class/Calc.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        protected double operand1;
+        protected double operand2;
+        protected string operatorSign;
+        protected bool isOperation;
+
+        protected Queue<double> memory;
+        protected List<double> numbers;
+
+        // default constructor
+        public Calc()
+        {
+            operand1 = 0.0;
+            operand2 = 0.0;
+            isOperation = false;
+
+            memory = new Queue<double>();
+            numbers = new List<double>();
+        }
+
+        // Menghapus semua yang ada di layar dan di memory kalkulator
+        // Dipanggil ketika tombol AC ditekan
+        public void Reset()
+        {
+            operand1 = 0.0;
+            operand2 = 0.0;
+            isOperation = false;
+
+            memory.Clear();
+            numbers.Clear();
+        }
+
+        // GETTER & SETTER
+
+        // Mengubah string yang diterima dari input kalkulator menjadi double
+        // kemudian menyimpannya ke operand
+        public void SetOperand1(string number)
+        {
+            operand1 = double.Parse(number);
+        }
+
+        public void SetOperand2(string number)
+        {
+            operand2 = double.Parse(number);
+        }
+
+        // Menyimpan operator dari input ke dalam operatorSign
+        public void SignOperator(string sign)
+        {
+            operatorSign = sign;
+        }
+
+        // Mengembalikan operator dari operatorSign
+        public string GetSignOperator()
+        {
+            return operatorSign;
+        }
+
+        // Mengubah nilai isOperation
+        // isOperation bernilai true jika yang barusan ditekan adalah tombol operator
+        public void SetStateOperation(bool state)
+        {
+            isOperation = state;
+        }
+
+        // Mengembalikan nilai isOperation
+        // isOperation bernilai true jika yang barusan ditekan adalah tombol operator
+        public bool GetStateOperation()
+        {
+            return isOperation;
+        }
+
+        // Memasukkan angka ke dalam memory
+        public void SetMemory(string nums)
+        {
+            memory.Enqueue(double.Parse(nums));
+        }
+
+        // Mengeluarkan angka dari memory
+        public double GetMemory()
+        {
+            return memory.Dequeue();
+        }
+
+        // Menghitung hasil operasi <operand1> <operator> <operand2>
+        public double calculate()
+        {
+            double temp = 0.0;
+            switch (operatorSign)
+            {
+                case "+":
+                    temp = operand1 + operand2;
+                    break;
+
+                case "-":
+                    temp = operand1 - operand2;
+                    break;
+
+                case "*":
+                    temp = operand1 * operand2;
+                    break;
+
+                case "/":
+                    temp = operand1 / operand2;
+                    break;
+            }
+            return temp;
+        }
+    }
+}
